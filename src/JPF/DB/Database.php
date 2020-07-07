b5ae24ed6af07d8ddea5db4e04317eccd5d395a5
Juliano Pantoni Filho <juliano.88@Hotmail.com>
2020-07-06 22:47:21 -0300

    Config database

---
+++ src/JPF/DB/Database.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace JPF\DB;
+
+use PDO, PDOException;
+
+// Conexão
+define('HOST', 'localhost');  
+define('DBNAME', 'teste_cohros');  
+define('CHARSET', 'utf8');  
+define('USER', 'root');  
+define('PASSWORD', '');  
+
+class Database 
+{
+    private static $pdo;
+
+    function getConnection() {
+        if (!isset(self::$pdo)) {  
+            try {  
+                $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8', PDO::ATTR_PERSISTENT => TRUE);  
+                self::$pdo = new PDO("mysql:host=" . HOST . "; dbname=" . DBNAME . "; charset=" . CHARSET . ";", USER, PASSWORD, $options);  
+            } catch (PDOException $e) {  
+                print "Error: " . $e->getMessage();  
+            }  
+        }  
+        return self::$pdo; 
+    }
+
+}
