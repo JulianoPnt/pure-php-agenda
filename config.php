2e18812dfc730bad167c9035ddad9192faee7af9
Juliano Pantoni Filho <juliano.88@Hotmail.com>
2020-07-14 18:22:48 -0300

    Config default for php -s localhost:8000

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 return [
     //Paths
     'LOG_PATH' => __DIR__ . './logs',
-    'DIRECTORY' => '/',
+    'DIRECTORY' => '',
     //Database
     'DB_HOST' => 'localhost',
     'DB_NAME' => 'teste_cohros',
