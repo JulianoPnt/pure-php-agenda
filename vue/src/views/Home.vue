84d587fc39269f5bc1d2aa7c9f31115437e09b5f
Juliano Pantoni Filho <juliano.88@Hotmail.com>
2020-07-15 08:11:18 -0300

    Handle first request

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -8,7 +8,32 @@
 
 <script>
 export default {
+  methods: {
+    checkToken() {
+      return this.$http({
+          url: this.api_url + 'auth/checktoken',
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + localStorage.getItem('user_token')
+          },
+      })
+      .then(response => {
+          console.log(response);
+          this.$router.push('/contacts');
 
+      })
+      .catch(error => {
+          console.log(error);
+          localStorage.removeItem('user_token');
+          localStorage.removeItem('expires_at');
+          this.$router.push('/login');
+      });
+    }
+  },
+  created() {
+    this.checkToken();
+  }
 }
 </script>
 
