b4f28174953e1dc60b38c395efc3c72aaeec4f2d
Juliano Pantoni Filho <juliano.88@Hotmail.com>
2020-07-15 20:48:47 -0300

    Validation fix

--- app/Controller/AgendaController.php
+++ app/Controller/AgendaController.php
@@ -91,7 +91,7 @@ class AgendaController
             'address_city'          => 'alpha_spaces|min:5|max:40',
             'address_state'         => 'alpha_spaces|min:5|max:40',
             'address'               => 'alpha_spaces|min:5|max:40',
-            'address_number'        => 'integer|min:5',
+            'address_number'        => 'integer',
             'address_cep'           => 'alpha_dash|min:5|max:20',
             'address_district'      => 'alpha_spaces|min:5|max:40',
             'phones'                => 'array',
